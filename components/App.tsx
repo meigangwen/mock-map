f05662925d5f7f9d6ed3ef4c096795560caffccf
fancilivain <meigangwen@gmail.com>
2023-08-21 23:23:26 +0800

    added geojson data for mock drawing

--- components/App.tsx
+++ components/App.tsx
@@ -2,10 +2,35 @@
 
 import { Canvas } from '@react-three/fiber'
 import { MapControls } from '@react-three/drei'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import Svg from '@/components/Svg'
 
+
 export default function App() {
+    
+    
+    function fetchJSON(url) {
+        return fetch(url)
+          .then(function(response) {
+            return response.json();
+          })
+    }
+
+    useEffect(() => {
+        var geoData = fetchJSON('data/addresses.geojson')
+            .then(function(data) { 
+
+        data.features.forEach(function(feature) {
+                console.log(feature)
+                var symbol = feature.properties['icon']
+                console.log(symbol)
+        })
+
+    })
+
+    },[])
+
+    
     return (
         <Canvas 
             frameloop="demand" 
