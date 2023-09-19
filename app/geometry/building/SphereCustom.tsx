5e5585c554c63a4113b905e8e183045c8842f24b
fancilivain <meigangwen@gmail.com>
2023-09-19 21:11:55 +0800

    Update SphereCustom.tsx

--- app/geometry/building/SphereCustom.tsx
+++ app/geometry/building/SphereCustom.tsx
@@ -14,15 +14,14 @@ export default function SphereCustom(){
                     fragmentShader={fragmentShader}
                     side={THREE.FrontSide}
                     //uniforms = {THREE.ShaderLib.lambert.uniforms}
-                    
+
                     uniforms={{ 
                                 diffuse:{value: new THREE.Color( 0xff0000 )},
                                 emissive:{value: new THREE.Color( 0x000000 )},
                                 opacity: { value: 1.0 },
                                 ambientLightColor: { value: new THREE.Color( 0xffffff ) },
                                 receiveShadow: {value: true },
-                                lightColor: {value: new THREE.Color( 0xffffff )},
-                                lightDirection: { value: new THREE.Vector3(0, 0, 1) }, 
+                                directionalLights: {value: new THREE.DirectionalLight(0xffffff, 1.0)}
                              }} 
                 /> 
                 <sphereGeometry args={[5.0]} />
