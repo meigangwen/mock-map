ad8a213a548aea12c8cafdabf419820d296c521e
fancilivain <meigangwen@gmail.com>
2023-09-26 10:19:52 +0800

    convert landcover into 1 component

--- app/vectorTile2/components/Landcover.tsx
+++ app/vectorTile2/components/Landcover.tsx
@@ -5,29 +5,79 @@ import { ringToShape, ringToHole, signedArea } from '../functions/Polygon'
 
 export default function Landcover({landcoverLayer}) {
 
-    //const [landcoverList, setLandcoverList] = useState([])
     const [shapes, setShapes] = useState()
+    const [hovered, setHovered] = useState()
 
     const { visible } = useControls("Landcover", {
         visible: true,
     })
 
+    // defines the class name list
+    const landClass = ['grass', 'wood', 'sand']
+
     useEffect(() => {
+        
+        let shapes,
+            shape
 
         for (let i=0; i<landcoverLayer.length; i++){
             // looping through the features
 
-            //landcoverList.push(landcoverLayer.feature(i))
-            //console.log(landcoverLayer.feature(i).properties.class)
+            
+            const geometry = landcoverLayer.feature(i).loadGeometry()
+            
+            if (geometry.length === 1) {
+                // there is no need to check for holes
+                const ring = geometry[0]
+                shape = ringToShape(ring)
+                grassShapes.push(shape)
+            }
+            
+            else {
+                // need to run test to check for holes
+                for (let i = 0; i < geometry.length; i++){
+                    const ring = geometry[i]
+                    const area = signedArea(ring)
+                    if  (area > 0 ){
+                        // this area is a shape
+                        shape = ringToShape(ring)
+                        grassShapes.push(shape)
+                    }
+                    if ( area < 0 ){
+                        // this area is a hole, which needs to be attached to the previous shape
+                        const hole = ringToHole(ring)
+                        shape?.holes.push(hole)
+                        grassShapes.pop()
+                        grassShapes.push(shape)
+                    }
+                }
+            
+            }
         }
-        
+        setShapes(grassShapes)
     }, [])
 
     return (
         <group visible={visible} >
+            <mesh
+                position = {[0,0,0]}
+                renderOrder={2}
+                receiveShadow
+                onPointerOver={(e) => {
+                    setHovered(true)
+                    e.stopPropagation()
+                }} 
+                onPointerOut={() => {
+                    setHovered(false)  
+                }} >
+                <meshStandardMaterial 
+                    color={hovered? 'red':'#ffffff'} 
+                    side={THREE.FrontSide} 
+                    depthTest={false} />
+                <shapeGeometry args={[shapes]} />
+            </mesh>
         </group>
     )
-      
 }
 
 
--- app/vectorTile2/components/Water.tsx
+++ app/vectorTile2/components/Water.tsx
@@ -116,4 +116,3 @@ export default function Water({waterLayer}) {
         </group>
     )
 }
-
