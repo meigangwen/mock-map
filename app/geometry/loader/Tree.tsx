57d87373175929672b53453b2f8943fb81df69f7
fancilivain <meigangwen@gmail.com>
2023-12-29 14:06:18 +0800

    made some changes

--- app/geometry/loader/Tree.tsx
+++ app/geometry/loader/Tree.tsx
@@ -5,7 +5,8 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 //import { perf}
 
 export default function Tree() {
-  const gltf = useLoader(GLTFLoader, "/model/tree01/scene.gltf");
+  const gltf = useLoader(GLTFLoader, "/model/houdini/tree_01.glb");
+  //const gltf = useLoader(GLTFLoader, "/model/tree01/scene.gltf");
 
   //<primitive object={gltf.scene} position={[0, 0, 0]} />
   return <primitive object={gltf.scene} position={[0, 0, 0]} />;
