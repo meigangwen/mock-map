14a96ee9024ef4c7814e40238cb98ae82b92b9cb
fancilivain <meigangwen@gmail.com>
2024-01-03 15:38:39 +0800

    modified something

--- app/geometry/loader/Houdini.tsx
+++ app/geometry/loader/Houdini.tsx
@@ -5,7 +5,7 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 //import { perf}
 
 export default function Houdini() {
-  const gltf = useLoader(GLTFLoader, "/model/houdini/HDB_01.gltf");
+  const gltf = useLoader(GLTFLoader, "/model/houdini/14_12914_8132.glb");
 
   //<primitive object={gltf.scene} position={[0, 0, 0]} />
   return <primitive object={gltf.scene} position={[0, 0, 0]} />;
