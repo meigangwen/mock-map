a91e709d69ab5b7bfe9e1477da3788a64650c62a
fancilivain <meigangwen@gmail.com>
2024-01-03 17:32:59 +0800

    mm

--- app/geometry/loader/Houdini.tsx
+++ app/geometry/loader/Houdini.tsx
@@ -5,7 +5,7 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 //import { perf}
 
 export default function Houdini() {
-  const gltf = useLoader(GLTFLoader, "/model/houdini/14_12914_8132.glb");
+  const gltf = useLoader(GLTFLoader, "/model/houdini/trees_01.glb");
 
   //<primitive object={gltf.scene} position={[0, 0, 0]} />
   return <primitive object={gltf.scene} position={[0, 0, 0]} />;
