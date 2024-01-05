64abfce27ec41f69bef94520dbb6d8ec3d253967
fancilivain <meigangwen@gmail.com>
2024-01-05 17:51:01 +0800

    managed to extract model from gltf file

--- app/geometry/instancing/Houdini.tsx
+++ app/geometry/instancing/Houdini.tsx
@@ -5,8 +5,28 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 //import { perf}
 
 export default function Houdini() {
-  const gltf = useLoader(GLTFLoader, "/model/houdini/trees_01.glb");
+  const gltf = useLoader(GLTFLoader, "/model/houdini/tree_01.glb");
+  let geometry = new THREE.BufferGeometry();
 
-  //<primitive object={gltf.scene} position={[0, 0, 0]} />
-  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
+  gltf.scene.traverse(function (child) {
+    if (child.isMesh) {
+      //retrieve geometry/map/skeleton or whatsoever here...
+      //if there is not vertex normals, compute it
+      if (!child.geometry.attributes.normal) {
+        child.geometry.computeVertexNormals();
+      }
+      geometry = child.geometry;
+      console.log(child);
+    }
+  });
+
+  //return <primitive object={gltf.scene} position={[0, 0, 0]} />;
+  //return <primitive object={gltf.scene} position={[0, 0, 0]} />;
+  //<meshStandardMaterial envMapIntensity={0.2} vertexColors={true} />
+
+  return (
+    <mesh geometry={geometry}>
+      <meshStandardMaterial envMapIntensity={1.0} vertexColors={true} />
+    </mesh>
+  );
 }
