0db47e99978b1ce197079a983b6dda48fe3aabf8
fancilivain <meigangwen@gmail.com>
2024-01-08 11:14:41 +0800

    made instancing of trees working in a single test scene

--- app/geometry/instancing/Houdini.tsx
+++ app/geometry/instancing/Houdini.tsx
@@ -1,48 +1,15 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 import * as THREE from "three";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-
-/*
-function InstancedThing() {
-  const ref = useRef()
-  useEffect(() => {
-    ref.current.setMatrixAt(0, new THREE.Matrix4())
-  }, [])
-  return (
-    <instancedMesh ref={ref} args={[null, null, 1]}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshNormalMaterial />
-    </instancedMesh>
-  )
-}
-*/
-
-/*
-function InstancedThing() {
-  const ref = useUpdate(imesh => {
-    imesh.setMatrixAt(0, new THREE.Matrix4())
-  }, [])
-  return (
-    <instancedMesh ref={ref} args={[null, null, 1]}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshNormalMaterial />
-    </instancedMesh>
-  )
-}
-*/
 
 export default function Houdini() {
   const ref = useRef();
   const gltf = useLoader(GLTFLoader, "/model/houdini/tree_01.glb");
 
-  useEffect(() => {
-    //ref.current.setMatrixAt(0, new THREE.Matrix4());
-  }, []);
-
   let geometry = new THREE.BufferGeometry();
-  let instancedMesh;
   let material;
+  const instanceCount = 1000;
 
   gltf.scene.traverse(function (child) {
     if (child.isMesh) {
@@ -53,27 +20,27 @@ export default function Houdini() {
 
       geometry = child.geometry;
       material = child.material;
-
-      const instanceCount = 10;
-      instancedMesh = new THREE.InstancedMesh(
-        geometry,
-        material,
-        instanceCount
-      );
-
-      const matrix = new THREE.Matrix4();
-      for (let i = 0; i < instanceCount; i++) {
-        matrix.setPosition(
-          Math.random() * 50,
-          Math.random() * 50,
-          Math.random() * 50
-        );
-        instancedMesh.setMatrixAt(i, matrix);
-      }
     }
   });
 
+  useLayoutEffect(() => {
+    const matrix = new THREE.Matrix4();
+    for (let i = 0; i < instanceCount; i++) {
+      matrix.setPosition(
+        Math.random() * 500,
+        Math.random() * 500,
+        Math.random() * 500
+      );
+      ref.current.setMatrixAt(i, matrix);
+    }
+  }, []);
+
   return (
-    <instancedMesh geometry={instancedMesh} material={material}></instancedMesh>
+    <instancedMesh
+      ref={ref}
+      args={[null, null, instanceCount]}
+      material={material}
+      geometry={geometry}
+    ></instancedMesh>
   );
 }
